6c8bd809985aa9ce6a4254c1681ab935d9687fa3
Ioan Zaharia <zaharia.ioan@gmail.com>
2022-05-25 13:23:45 +0200

    basic POST request with fs

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,53 @@
+import { useRef } from 'react';
+
 function HomePage() {
+  const emailInputRef = useRef();
+  const feedbackInputRef = useRef();
+
+  function submitFormHandler(e) {
+    e.preventDefault();
+
+    const enteredEmail = emailInputRef.current.value;
+    const enteredFeedback = emailInputRef.current.value;
+
+    const reqBody = {
+      email: enteredEmail,
+      text: enteredFeedback,
+    };
+
+    fetch('/api/feedback', {
+      method: 'POST',
+      body: JSON.stringify(reqBody),
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    })
+      .then((response) => response.json())
+      .then((data) => console.log(data));
+  }
+
   return (
     <div>
       <h1>The Home Page</h1>
+      <form onSubmit={submitFormHandler}>
+        <div>
+          <label htmlFor='email'>Your email address</label>
+          <br />
+          <input type='email' id='email' ref={emailInputRef} />
+        </div>
+        <br />
+        <div>
+          <label htmlFor='feedback'>Your feedback</label>
+          <br />
+          <textarea
+            id='feedback'
+            rows='5'
+            placeholder='Your feedback'
+            ref={feedbackInputRef}
+          />
+        </div>
+        <button type='submit'>Send Feedback</button>
+      </form>
     </div>
   );
 }
